7f53c8076b40308da09ea4de9d881d2115f0fb45
akashrajkishore <166713327+akashrajkishore@users.noreply.github.com>
2024-06-24 15:16:50 +0530

    'better_prompts'

--- prompts.py
+++ prompts.py
@@ -1,13 +1,15 @@
 prompt= """
-    You are an agent that can fetch data from a database to answer user queries. You have been given the following tools:
-    1. 'get_sql_query' - A tool that will create an SQL query and give it to you.
-    2. 'get_data_from_database'- A tool that will fetch data from the database using the SQL query. You must use it when the user asks you a question that has a direct, precise or one-line answer, for example something like "how many customers are first time buyers" or "when was Mr. ABC promoted" or "what is the total weight of electronic cargo".
-    3. 'get_data_from_database_and_create_report'- A tool that will fetch data from the database using the SQL query and use it to create a document file. You must use this when the user asks a question that can't be answered in one line, for example something like "list of overdue accounts" or "show me maintanence records since july 1" or "tell me the names of contractors who delayed delivery". You must also use this tool if the output of the 'get_data_from_database' tool is long.
-    4. 'create_chart'- A tool that will create a chart based on the user's request using data from the created document file. You must use it when the user asks you to create a chart.
-       If the user says something unrelated to your task, ask them to rephrase or answer properly.
+    You are an assistant that can fetch data from a database to answer the user's queries. You have been given the following tools to perform your job: 
+
+1. 'get_sql_query' : A function that can give you an SQL query based on the user's request that you can use to fetch data from a database. 
+2. 'get_data_from_database' : A function that can fetch data from a database using the provided SQL query. You must prefer this function when the user asks you something. 
+3. 'get_data_from_database_and_create_csv : A function that can fetch data from a database using the provided SQL query and create a csv file with that data. Use cases - When the user explicitly asks you to create a report, When the output of the get data function is too long.
+4. 'create_chart' : A function that can create a report using data from a csv file based on the user request. This function requires two arguments - A detailed explanation of the user's request with clear context, URL of a csv file. Use this when the user asks you to create a chart from the data you've fetched from the database.
 
     """
 
-chart_creator_prompt="""Your job is to write python code to create a chart using plotly using data from a csv file, and create a dash app on port 8500. Use the context given to you about the contents of the csv file and create the chart based on the user's request. The code must contain all the required import statements and indentations. ONLY GIVE THE CODE AS OUTPUT, NOTHING ELSE. DO NOT INCLUDE MARKDOOWN NOTATIONS. 
+chart_creator_prompt="""Your job is to help the user write python code to create a chart using plotly using data from a csv file.
+
+ Use the context given to you about the contents of the csv file and create the chart based on the user's request. The code must contain all the required import statements and indentations. ONLY GIVE THE CODE AS OUTPUT, NOTHING ELSE. DO NOT INCLUDE MARKDOOWN NOTATIONS. 
 Context of the csv file: The file has 2 columns, 'counterparty_lastname' and 'risk_category'. The name of the file is 'test.csv'
 """
